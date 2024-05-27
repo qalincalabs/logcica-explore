40a370ea2475eb2cfc8023994add8735a9dd3d23
Olivier Wouters <olivier5741@gmail.com>
2024-05-27 12:11:13 +0200

    Centralize collection list configuration

---
+++ collections.ts
@@ -0,0 +1,22 @@
+export const collections: string[] = [
+  "products",
+  "organisations",
+  "workspaces",
+  "activities",
+  "categories",
+  "availabilities",
+  "season_availabilities", // WARNING
+  "codes",
+  "codeLists",
+  "places",
+  "units",
+  "information_systems", // WARNING
+  "references",
+  "counters",
+  "catalogs",
+  "profiles",
+  "partnerships",
+  "contributions",
+  "actions",
+  "contacts"
+];
