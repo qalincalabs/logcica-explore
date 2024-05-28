8c4ce6ecc2356dd202dcb4ad6dec2e3c9b880d60
Olivier Wouters <olivier5741@gmail.com>
2024-05-28 17:29:01 +0200

    More info into algolia

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -1,16 +1,48 @@
 const activitiesQuery = `
-  {
-    pages: allMongodbActivities {
+{
+    activities: allMongodbActivities {
       nodes {
         _id
         name
         type
+        categories {
+          name
+        }
+        distributionsCategories {
+          name
+        }
         description {
           short {
             markdown
           }
         }
+        place {
+          ...PlaceFields
+        }
+        catalogs {
+          name
+          productCategories {
+            name
+          }
+          description {
+            short {
+              markdown
+            }
+          }
+        }
       }
+    }
+  }
+  
+  fragment PlaceFields on mongodbPlaces {
+    center {
+      coordinates
+    }
+    address {
+      locality
+    }
+    within {
+      name
     }
   }
 `;
@@ -18,18 +50,22 @@ const activitiesQuery = `
 const queries = [
   {
     query: activitiesQuery,
-    transformer: ({ data }) => data.pages.nodes.map(n => ({
-        objectID: n?._id,
-        frontmatter: {
-            title: n?.name
-        },
-        fields: {
-            type: n?.type
-        },
-        excerpt: n?.description?.short?.markdown
-    })),
+    transformer: ({ data }) => data.activities.nodes.map(n => {
+        n.objectID = n._id
+        delete n._id
+        if(n.place?.center?.coordinates){
+            n._geoloc = {
+              lat: n.place.center.coordinates[1],
+              lng: n.place.center.coordinates[0]
+            }
+            delete n.place.center
+        }
+        return n
+    }),
     indexName: "Activities",
-    settings: { attributesToSnippet: [`excerpt:20`] },
+    settings: { 
+        //attributesToSnippet: [`description.short.markdown:20`] 
+    },
   },
 ];
 
