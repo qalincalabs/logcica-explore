765d934c86d2cf2a06d8e85c0c97a1eaed2730c4
ysrhad03 <haddougayassir@gmail.com>
2024-06-26 12:03:38 +0200

    responsive filter

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -15,8 +15,10 @@ import {
   ListItemIcon,
   FormControlLabel,
   Checkbox,
-  Divider
+  Divider,
+  FormGroup
 } from "@mui/material";
+import { styled } from '@mui/material/styles';
 import Layout from "../components/layout";
 import { Store, Star, StarBorder, Favorite, Delete } from "@mui/icons-material";
 import Markdown from "markdown-to-jsx";
@@ -24,6 +26,33 @@ import Markdown from "markdown-to-jsx";
 type favoriteItem = {
   targetId: string
 }
+
+const CustomCheckbox = styled(Checkbox)(({ theme }) => ({
+  '&.Mui-checked': {
+    color: 'black',
+  },
+  '&.MuiCheckbox-root': {
+    color: 'white',
+  },
+  '& .MuiSvgIcon-root': {
+    fontSize: 28,
+  },
+  '&:hover': {
+    backgroundColor: 'rgba(0, 0, 0, 0.04)',
+    borderRadius: '50%',
+  },
+  transition: theme.transitions.create(['background-color', 'border']),
+}));
+
+const FilterBox = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(2),
+  border: '1px solid lightgrey',
+  borderRadius: 8,
+  backgroundColor: '#FFD700',
+  display: 'flex',
+  alignItems: 'center',
+  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+}));
 
 const MarketplacePage: React.FC<PageProps> = ({ data }: any) => {
   const [favorites, setFavorites] = useState<favoriteItem[]>([]);
@@ -36,13 +65,11 @@ const MarketplacePage: React.FC<PageProps> = ({ data }: any) => {
   }, []);
 
   const toggleFavorite = (id: string) => {
-    const updatedFavorites = favorites.some(fav => fav.targetId == id)
+    const updatedFavorites = favorites.some(fav => fav.targetId === id)
       ? favorites.filter(fav => fav.targetId !== id)
-      : [...favorites, {targetId: id}];
-    
-    console.log(updatedFavorites)
+      : [...favorites, { targetId: id }];
+
     setFavorites(updatedFavorites);
-    console.log(updatedFavorites)
     localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
   };
 
@@ -65,7 +92,7 @@ const MarketplacePage: React.FC<PageProps> = ({ data }: any) => {
   };
 
   const filteredMarketplaces = showFavoritesOnly
-    ? data.marketplaces.nodes.filter((m: any) => favorites.some(fav => fav.targetId == m._id))
+    ? data.marketplaces.nodes.filter((m: any) => favorites.some(fav => fav.targetId === m._id))
     : data.marketplaces.nodes;
 
   return (
@@ -78,13 +105,12 @@ const MarketplacePage: React.FC<PageProps> = ({ data }: any) => {
           <Favorite />
         </IconButton>
       </Box>
-      <Box display="flex">
-        <Box width="250px" p={2} mr={2} borderRight="1px solid #ccc" bgcolor="lightgrey">
-          <Typography variant="h6" style={{ color: 'black', fontWeight: 'bold' }}>Filtres</Typography>
-          <Divider />
+      <Box display="flex" justifyContent="center" alignItems="center" mb={4}>
+        <FilterBox>
+          <Typography variant="h6" style={{ color: 'black', fontWeight: 'bold', marginRight: '16px' }}>Filtres</Typography>
           <FormControlLabel
             control={
-              <Checkbox
+              <CustomCheckbox
                 checked={showFavoritesOnly}
                 onChange={handleShowFavoritesOnlyChange}
                 name="showFavoritesOnly"
@@ -92,47 +118,47 @@ const MarketplacePage: React.FC<PageProps> = ({ data }: any) => {
             }
             label={<Typography style={{ color: 'black' }}>Favoris</Typography>}
           />
-        </Box>
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <List>
-            {filteredMarketplaces.map((m: any) => (
-              <ListItem key={m._id}>
-                <ListItemButton onClick={() => navigate("/marketplace/" + m._id)}>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <Store />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={m.name}
-                    secondary={
-                      <Stack>
-                        <Typography>
-                          {m.availabilityStatement.short.markdown}
-                        </Typography>
-                        {m.description && (
-                          <Markdown>{m.description.short.markdown}</Markdown>
-                        )}
-                      </Stack>
-                    }
-                  />
-                </ListItemButton>
-                <ListItemIcon>
-                  <IconButton onClick={() => toggleFavorite(m._id)}>
-                    {favorites.some(fav => fav.targetId == m._id) ? <Star /> : <StarBorder />}
-                  </IconButton>
-                </ListItemIcon>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
+        </FilterBox>
+      </Box>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <List>
+          {filteredMarketplaces.map((m: any) => (
+            <ListItem key={m._id}>
+              <ListItemButton onClick={() => navigate("/marketplace/" + m._id)}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Store />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={m.name}
+                  secondary={
+                    <Stack>
+                      <Typography>
+                        {m.availabilityStatement.short.markdown}
+                      </Typography>
+                      {m.description && (
+                        <Markdown>{m.description.short.markdown}</Markdown>
+                      )}
+                    </Stack>
+                  }
+                />
+              </ListItemButton>
+              <ListItemIcon>
+                <IconButton onClick={() => toggleFavorite(m._id)}>
+                  {favorites.some(fav => fav.targetId === m._id) ? <Star /> : <StarBorder />}
+                </IconButton>
+              </ListItemIcon>
+            </ListItem>
+          ))}
+        </List>
       </Box>
       <Drawer anchor="right" open={drawerOpen} onClose={handleDrawerClose}>
         <Box p={2} width={250}>
           <Typography variant="h6" style={{ color: 'black' }}>Favoris</Typography>
           <List>
             {data.marketplaces.nodes
-              .filter((m: any) => favorites.some(fav => fav.targetId == m._id))
+              .filter((m: any) => favorites.some(fav => fav.targetId === m._id))
               .map((m: any) => (
                 <ListItem key={m._id}>
                   <ListItemButton onClick={() => navigate("/marketplace/" + m._id)}>
