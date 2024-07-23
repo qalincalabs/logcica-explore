4cd7ff3d620e7e647a761e6746cead0101454bfb
Olivier Wouters <olivier5741@gmail.com>
2024-07-23 13:11:57 +0200

    Simplify layout (make it easier to maintain later)

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -17,7 +17,6 @@ import {
   Hidden,
   CssBaseline,
   useTheme,
-  useMediaQuery,
 } from "@mui/material";
 import { Store, Menu } from "@mui/icons-material";
 import Layout from "../components/layout";
@@ -25,7 +24,6 @@ import * as favoriteService from "../utils/favoritesService";
 
 const SharePage = ({ data }) => {
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const [sharedFavorites, setSharedFavorites] = useState(null);
   const [listName, setListName] = useState("Partagés");
@@ -39,6 +37,8 @@ const SharePage = ({ data }) => {
     }
   }, []);
 
+
+  // TODO: directly create a method in favoritesService to create a list from shared content
   const handleAddToFavorites = () => {
     if (sharedFavorites) {
       const newList = favoriteService.addList({ name: listName });
@@ -59,51 +59,15 @@ const SharePage = ({ data }) => {
     }
   };
 
-  const filters = [
-    { key: 'partnership', title: 'Groupements', dataKey: 'partnership', dataNodes: data?.partnerships?.nodes || [] },
-    { key: 'counter', title: 'Marchés', dataKey: 'counter', dataNodes: data?.marketplaces?.nodes || [] },
-    { key: 'activity', title: 'Producteurs', dataKey: 'activity', dataNodes: data?.activities?.nodes || [] },
-    { key: 'product', title: 'Produits', dataKey: 'product', dataNodes: data?.products?.nodes || [] },
-  ];
-
   if (!sharedFavorites) return <Typography>Chargement...</Typography>;
 
   return (
     <Layout>
       <CssBaseline />
       <Grid container sx={{ height: '100vh' }}>
-        <Hidden smDown>
-          <Grid item xs={12} md={3} lg={2}>
-            <Box sx={{ height: '100%', bgcolor: 'lightgray' }}>
-              <Box p={2} width="100%">
-                <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2, color: '#000', textAlign: 'center' }}>Favoris partagés</Typography>
-                <Button variant="contained" color="primary" onClick={handleAddToFavorites} fullWidth>
-                  Ajouter à mes favoris
-                </Button>
-              </Box>
-            </Box>
-          </Grid>
-        </Hidden>
-        <Hidden mdUp>
-          <Drawer anchor="left" open={false} onClose={() => {}}>
-            <Box sx={{ height: '100%', bgcolor: 'lightgray', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Box p={2} width="100%">
-                <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2, color: '#000', textAlign: 'center' }}>Favoris partagés</Typography>
-                <Button variant="contained" color="primary" onClick={handleAddToFavorites} fullWidth>
-                  Ajouter à mes favoris
-                </Button>
-              </Box>
-            </Box>
-          </Drawer>
-        </Hidden>
         <Grid item xs={12} md={9} lg={10} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
           <Box>
             <Box display="flex" alignItems="center" mb={2}>
-              <Hidden smUp>
-                <IconButton onClick={() => {}}>
-                  <Menu />
-                </IconButton>
-              </Hidden>
               <Typography align="center" variant="h3" my={4} sx={{ flexGrow: 1, fontWeight: 'bold', color: '#FFD700' }}>Favoris partagés</Typography>
             </Box>
             <Box display="flex" justifyContent="center" my={2}>
@@ -111,46 +75,7 @@ const SharePage = ({ data }) => {
                 Ajouter à mes favoris
               </Button>
             </Box>
-            <Box p={2} width="100%" display="flex" justifyContent="center" flexDirection="column" alignItems="center">
-              <Grid container spacing={2} justifyContent="center" alignItems="flex-start">
-                {filters.map(({ title, dataKey, dataNodes }) => (
-                  sharedFavorites[dataKey]?.length > 0 && (
-                    <Grid item xs={12} key={dataKey}>
-                      <Box sx={{ mb: 3, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                        <Typography variant="h6" sx={{ color: '#333', mb: 2 }}>{title}</Typography>
-                      </Box>
-                      <List>
-                        {sharedFavorites[dataKey].map(item => {
-                          const dataNode = dataNodes.find(p => p._id === item.id);
-                          return (
-                            <ListItem 
-                              key={item.id} 
-                              sx={{ 
-                                transition: 'transform 0.3s, box-shadow 0.3s', 
-                                '&:hover': { transform: 'scale(1.02)', boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.2)' },
-                                borderRadius: '8px',
-                                mb: 2,
-                                bgcolor: '#fff',
-                                boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)'
-                              }}
-                            >
-                              <ListItemButton>
-                                <ListItemAvatar>
-                                  <Avatar sx={{ bgcolor: '#FFD700', color: '#fff' }}>
-                                    <Store />
-                                  </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary={dataNode?.name || `${title} inconnu`} sx={{ color: '#555' }} />
-                              </ListItemButton>
-                            </ListItem>
-                          );
-                        })}
-                      </List>
-                    </Grid>
-                  )
-                ))}
-              </Grid>
-            </Box>
+            {favoriteList(data, sharedFavorites)}
           </Box>
         </Grid>
       </Grid>
@@ -194,3 +119,54 @@ export const query = graphql`
     }
   }
 `;
+
+function favoriteList(data, sharedFavorites: never) {
+  
+  const filters = [
+    { key: 'partnership', title: 'Groupements', dataKey: 'partnership', dataNodes: data?.partnerships?.nodes || [] },
+    { key: 'counter', title: 'Marchés', dataKey: 'counter', dataNodes: data?.marketplaces?.nodes || [] },
+    { key: 'activity', title: 'Producteurs', dataKey: 'activity', dataNodes: data?.activities?.nodes || [] },
+    { key: 'product', title: 'Produits', dataKey: 'product', dataNodes: data?.products?.nodes || [] },
+  ];
+  
+  return <Box p={2} width="100%" display="flex" justifyContent="center" flexDirection="column" alignItems="center">
+    <Grid container spacing={2} justifyContent="center" alignItems="flex-start">
+      {filters.map(({ title, dataKey, dataNodes }) => (
+        sharedFavorites[dataKey]?.length > 0 && (
+          <Grid item xs={12} key={dataKey}>
+            <Box sx={{ mb: 3, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Typography variant="h6" sx={{ color: '#333', mb: 2 }}>{title}</Typography>
+            </Box>
+            <List>
+              {sharedFavorites[dataKey].map(item => {
+                const dataNode = dataNodes.find(p => p._id === item.id);
+                return (
+                  <ListItem
+                    key={item.id}
+                    sx={{
+                      transition: 'transform 0.3s, box-shadow 0.3s',
+                      '&:hover': { transform: 'scale(1.02)', boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.2)' },
+                      borderRadius: '8px',
+                      mb: 2,
+                      bgcolor: '#fff',
+                      boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)'
+                    }}
+                  >
+                    <ListItemButton>
+                      <ListItemAvatar>
+                        <Avatar sx={{ bgcolor: '#FFD700', color: '#fff' }}>
+                          <Store />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary={dataNode?.name || `${title} inconnu`} sx={{ color: '#555' }} />
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Grid>
+        )
+      ))}
+    </Grid>
+  </Box>;
+}
