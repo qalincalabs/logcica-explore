740b39f6929a43b8bfd3aff9cf28963025061065
BenjaminGuillaumat <120639167+BenjaminGuillaumat@users.noreply.github.com>
2024-07-31 16:27:46 +0200

    added 7 activity icons

--- src/assets/activity-icons.tsx
+++ src/assets/activity-icons.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import CuredMeatShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bacon.svg";
+import BreweryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/beer.svg";
 import BakeryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bread.svg";
-import OtherIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/food.svg";
-import ButcherIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/meat.svg";
+import ChocolateFactoryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/chocolate.svg";
+import BiscuitFactoryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cookie.svg";
+import BeekeepingIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/honey_pot.svg";
+import ConfectioneryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/lollipop.svg";
+import ButcherShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/meat.svg";
+import WineMerchantIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/wine.svg";
 
 // export const activityIcons = {
 //   bakery: <BakeryIcon />,
@@ -9,7 +15,14 @@ import ButcherIcon from "../assets/freepick/free-vector/drawing-cooking-color-ic
 // };
 
 export const activityIcons = {
-  bakery: <BakeryIcon />,
-  butcher_shop: <ButcherIcon />,
-  other: <OtherIcon />,
+  bakery: <BakeryIcon style={{ width: "1.3rem" }} />,
+  butcher_shop: <ButcherShopIcon style={{ width: "1.3rem" }} />,
+  beekeeping: <BeekeepingIcon style={{ width: "1.3rem" }} />,
+  biscuit_factory: <BiscuitFactoryIcon style={{ width: "1.3rem" }} />,
+  brewery: <BreweryIcon style={{ width: "1.3rem" }} />,
+  wine_merchant: <WineMerchantIcon style={{ width: "1.3rem" }} />,
+  cured_meat_shop: <CuredMeatShopIcon style={{ width: "1.3rem" }} />,
+  chocolate_factory: <ChocolateFactoryIcon style={{ width: "1.3rem" }} />,
+  confectionery: <ConfectioneryIcon style={{ width: "1.3rem" }} />,
+  other: <ConfectioneryIcon style={{ width: "1.3rem" }} />,
 };
