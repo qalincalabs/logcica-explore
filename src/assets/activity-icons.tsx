6364ce5dfac9cfe3e26e0ceb6b39f179b28e342e
BenjaminGuillaumat <120639167+BenjaminGuillaumat@users.noreply.github.com>
2024-07-31 17:10:39 +0200

    Added 11 more activity icons

--- src/assets/activity-icons.tsx
+++ src/assets/activity-icons.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import CuredMeatShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bacon.svg";
 import BreweryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/beer.svg";
 import BakeryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bread.svg";
+import PoultryBreedingIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/chicken.svg";
 import ChocolateFactoryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/chocolate.svg";
 import BiscuitFactoryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cookie.svg";
+import DairyFarmIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cow.svg";
+import FarmIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/farm.svg";
 import BeekeepingIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/honey_pot.svg";
 import ConfectioneryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/lollipop.svg";
 import ButcherShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/meat.svg";
 import WineMerchantIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/wine.svg";
+
+import CheeseDairyIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cheese.svg";
+import FishShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/fish.svg";
+import IceCreameryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/ice_cream.svg";
+import FlourMillIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/mill.svg";
+import ProducerIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/producer.svg";
+import MarketGardenIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/salad.svg";
+import CerealFarmIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/scarecrow.svg";
+import MarketIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/vegetables_basket.svg";
 
 // export const activityIcons = {
 //   bakery: <BakeryIcon />,
@@ -15,14 +27,25 @@ import WineMerchantIcon from "../assets/freepick/free-vector/drawing-cooking-col
 // };
 
 export const activityIcons = {
-  bakery: <BakeryIcon style={{ width: "1.3rem" }} />,
-  butcher_shop: <ButcherShopIcon style={{ width: "1.3rem" }} />,
-  beekeeping: <BeekeepingIcon style={{ width: "1.3rem" }} />,
-  biscuit_factory: <BiscuitFactoryIcon style={{ width: "1.3rem" }} />,
-  brewery: <BreweryIcon style={{ width: "1.3rem" }} />,
-  wine_merchant: <WineMerchantIcon style={{ width: "1.3rem" }} />,
-  cured_meat_shop: <CuredMeatShopIcon style={{ width: "1.3rem" }} />,
-  chocolate_factory: <ChocolateFactoryIcon style={{ width: "1.3rem" }} />,
-  confectionery: <ConfectioneryIcon style={{ width: "1.3rem" }} />,
-  other: <ConfectioneryIcon style={{ width: "1.3rem" }} />,
+  bakery: <BakeryIcon style={{ width: "1.8rem" }} />,
+  butcher_shop: <ButcherShopIcon style={{ width: "1.8rem" }} />,
+  beekeeping: <BeekeepingIcon style={{ width: "1.8rem" }} />,
+  biscuit_factory: <BiscuitFactoryIcon style={{ width: "1.8rem" }} />,
+  brewery: <BreweryIcon style={{ width: "1.8rem" }} />,
+  wine_merchant: <WineMerchantIcon style={{ width: "1.8rem" }} />,
+  cured_meat_shop: <CuredMeatShopIcon style={{ width: "1.8rem" }} />,
+  chocolate_factory: <ChocolateFactoryIcon style={{ width: "1.8rem" }} />,
+  confectionery: <ConfectioneryIcon style={{ width: "1.8rem" }} />,
+  farm: <FarmIcon style={{ width: "1.8rem" }} />,
+  dairy_farm: <DairyFarmIcon style={{ width: "1.8rem" }} />,
+  poultry_breeding: <PoultryBreedingIcon style={{ width: "1.8rem" }} />,
+  cheese_dairy: <CheeseDairyIcon style={{ width: "1.8rem" }} />,
+  ice_creamery: <IceCreameryIcon style={{ width: "1.8rem" }} />,
+  cereal_farm: <CerealFarmIcon style={{ width: "1.8rem" }} />,
+  market_garden: <MarketGardenIcon style={{ width: "1.8rem" }} />,
+  market: <MarketIcon style={{ width: "1.8rem" }} />,
+  flour_mill: <FlourMillIcon style={{ width: "1.8rem" }} />,
+  fish_shop: <FishShopIcon style={{ width: "1.8rem" }} />,
+  producer: <ProducerIcon style={{ width: "1.8rem" }} />,
+  other: <ProducerIcon style={{ width: "1.8rem" }} />,
 };
