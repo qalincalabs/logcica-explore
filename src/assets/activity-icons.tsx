441c38b46993270e5aca1244833fe34a128419bd
BenjaminGuillaumat <120639167+BenjaminGuillaumat@users.noreply.github.com>
2024-08-01 16:07:01 +0200

    added new activity icons, icon links and modified svg icon folders

--- src/assets/activity-icons.tsx
+++ src/assets/activity-icons.tsx
@@ -1,33 +1,38 @@
 import React from "react";
-import CuredMeatShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bacon.svg";
-import BreweryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/beer.svg";
-import BakeryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bread.svg";
-import CheeseDairyIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cheese.svg";
-import PoultryBreedingIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/chicken.svg";
-import ChocolateFactoryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/chocolate.svg";
-import BiscuitFactoryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cookie.svg";
-import DairyFarmIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cow.svg";
-import OtherIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/danger_panel.svg";
-import FarmIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/farm.svg";
-import FishShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/fish.svg";
-import BeekeepingIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/honey_pot.svg";
-import IceCreameryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/ice_cream.svg";
-import ConfectioneryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/lollipop.svg";
-import ButcherShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/meat.svg";
-import FlourMillIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/mill.svg";
-import ProducerIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/producer.svg";
-import MarketGardenIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/salad.svg";
-import CerealFarmIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/scarecrow.svg";
-import MarketIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/vegetables_basket.svg";
-import WineMerchantIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/wine.svg";
-
-import OrchardIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bucket_apples.svg";
-import CatererIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/caterer.svg";
-import FloristIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/flower.svg";
-import VineyardIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/grape.svg";
-import FruitProducerIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/producer_apple.svg";
-import BeverageProducerIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/producer_beverage.svg";
-import AnimalProducerIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/producer_chicken.svg";
+import CuredMeatShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bacon.svg"; //basics icons
+import BakeryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/bread.svg"; //basics icons
+import CheeseDairyIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cheese.svg"; //basics icons
+import BiscuitFactoryIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/cookie.svg"; //basics icons
+import FishShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/fish.svg"; //basics icons
+import VineyardIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/grape.svg"; //basics icons
+import ButcherShopIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/meat.svg"; //basics icons
+import MarketGardenIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/salad.svg"; //basics icons
+import MarketIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/vegetables_basket.svg"; //basics icons
+import WineMerchantIcon from "../assets/freepick/free-vector/drawing-cooking-color-icons_730579/wine.svg"; //basics icons
+import BreweryIcon from "../assets/freepick/other/beer.svg"; //https://fr.freepik.com/vecteurs-libre/icones-biere_767010.htm#fromView=search&page=1&position=0&uuid=bd402e54-61ec-40ef-86d3-bb83d8831786
+import OrchardIcon from "../assets/freepick/other/bucket_apples.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import CatererIcon from "../assets/freepick/other/caterer.svg"; //https://fr.freepik.com/vecteurs-libre/salle-manger-icone-icone-restaurant_1294547.htm#fromView=search&page=1&position=7&uuid=a5769177-58d4-410b-bc27-d7996784bca7
+import PoultryBreedingIcon from "../assets/freepick/other/chicken.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import ChocolateFactoryIcon from "../assets/freepick/other/chocolate.svg"; //https://fr.freepik.com/vecteurs-libre/icones-nourriture_959688.htm#fromView=search&page=1&position=34&uuid=d939a2b9-cbc5-4a60-8b6d-e053da9e860d
+import CooperativeFarmIcon from "../assets/freepick/other/cooperative.svg"; // https://www.freepik.com/free-vector/user-group-circles-multiple-colours_145857011.htm#fromView=search&page=3&position=21&uuid=33826e37-c14b-4fec-865f-6b7676ed86c9
+import DairyFarmIcon from "../assets/freepick/other/cow.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import OtherIcon from "../assets/freepick/other/danger_panel.svg"; //https://fr.freepik.com/vecteurs-libre/aucun-signe-n-est-indique_207467017.htm#fromView=search&page=1&position=34&uuid=07c25340-e7c1-454b-a961-eaf963dd9698
+import PoultryBreedingEggsIcon from "../assets/freepick/other/egg.svg"; //https://thenounproject.com/icon/egg-6442395/
+import FarmIcon from "../assets/freepick/other/farm.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import MeatFarmIcon from "../assets/freepick/other/farm_meat.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import FloristIcon from "../assets/freepick/other/flower.svg"; // https://fr.freepik.com/vecteurs-libre/ensemble-fleurs-colorees_3772108.htm#fromView=search&page=1&position=46&uuid=68e6f679-7dcb-4188-9144-b59738fd42d2
+import GroceryStoreIcon from "../assets/freepick/other/food.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import GoatFarmIcon from "../assets/freepick/other/goat.svg"; //https://fr.freepik.com/vecteurs-libre/ensemble-differentes-tetes-chevre-blanches_27289500.htm#fromView=search&page=2&position=24&uuid=2767bc54-d0dd-4856-916c-43d6df8ba613
+import BeekeepingIcon from "../assets/freepick/other/honey_pot.svg"; //https://fr.freepik.com/vecteurs-libre/icones-apiculture-du-miel-abeilles-dessines-main-nourriture-sucree-insecte-cellule-tonneau-nid-abeille_10600430.htm#fromView=search&page=1&position=25&uuid=3c4958fc-937d-4f5e-bc25-c81143809c32
+import IceCreameryIcon from "../assets/freepick/other/ice_cream.svg"; //https://fr.freepik.com/vecteurs-libre/icones-nourriture_959688.htm#fromView=search&page=1&position=34&uuid=d939a2b9-cbc5-4a60-8b6d-e053da9e860d
+import ConfectioneryIcon from "../assets/freepick/other/lollipop.svg"; //https://fr.freepik.com/vecteurs-libre/icones-nourriture_959688.htm#fromView=search&page=1&position=34&uuid=d939a2b9-cbc5-4a60-8b6d-e053da9e860d
+import FlourMillIcon from "../assets/freepick/other/mill.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import ProducerIcon from "../assets/freepick/other/producer.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import FruitProducerIcon from "../assets/freepick/other/producer_apple.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57 & basics icons
+import BeverageProducerIcon from "../assets/freepick/other/producer_beverage.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57 & basics icons
+import AnimalProducerIcon from "../assets/freepick/other/producer_chicken.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57 & basics icons
+import CerealFarmIcon from "../assets/freepick/other/scarecrow.svg"; //https://fr.freepik.com/vecteurs-libre/collection-icones-plat-menage-paysan-ferme-animaux-domestiques_3910295.htm#fromView=search&page=1&position=12&uuid=70c42894-1669-495a-8220-e5702fba3c57
+import DistilleryIcon from "../assets/freepick/other/scent_bottle.svg"; //https://fr.freepik.com/vecteurs-libre/elements-bien-etre-spa-dessines-main_40032821.htm#fromView=search&page=1&position=22&uuid=98a95de0-e76b-451d-8595-fe57c8715fe1
 
 // export const activityIcons = {
 //   bakery: <BakeryIcon />,
@@ -62,5 +67,14 @@ export const activityIcons = {
   caterer: <CatererIcon style={{ width: "1.8rem" }} />,
   orchard: <OrchardIcon style={{ width: "1.8rem" }} />,
   vineyard: <VineyardIcon style={{ width: "1.8rem" }} />,
+  grocery_store: <GroceryStoreIcon style={{ width: "1.8rem" }} />,
+  goat_farm: <GoatFarmIcon style={{ width: "1.8rem" }} />,
+  cooperative: <CooperativeFarmIcon style={{ width: "1.8rem" }} />,
+  creamery: <GroceryStoreIcon style={{ width: "1.8rem" }} />,
+  distillery: <DistilleryIcon style={{ width: "1.8rem" }} />,
+  poultry_breeding_eggs: (
+    <PoultryBreedingEggsIcon style={{ width: "1.8rem" }} />
+  ),
+  meat_farm: <MeatFarmIcon style={{ width: "1.8rem" }} />,
   other: <OtherIcon style={{ width: "1.8rem" }} />,
 };
